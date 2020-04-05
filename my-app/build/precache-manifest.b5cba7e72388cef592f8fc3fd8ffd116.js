20d52a42cf94dca87831c818fcdf06d33cd2a031
josetorres1223 <jose.torres@ufl.edu>
2020-04-05 00:27:44 +0000

    pushing my changes

---
+++ my-app/build/precache-manifest.b5cba7e72388cef592f8fc3fd8ffd116.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae795741c1cc60e0852efefdac50a676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfec57015e741b7c647",
+    "url": "/static/css/main.e1ddebd4.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfec57015e741b7c647",
+    "url": "/static/js/main.37b10471.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "c8ff2c80bc9631377817bc48796688e4",
+    "url": "/static/media/Circle.c8ff2c80.png"
+  }
+]);
